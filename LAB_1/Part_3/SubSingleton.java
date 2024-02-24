f37e9afba78591ad826806dc6ba04004f13305e7
Ejder Aysun <ejderaysunn@gmail.com>
2024-02-24 17:56:42 +0300

    Lab 1, part 3 solution added

---
+++ LAB_1/Part_3/SubSingleton.java
@@ -0,0 +1,9 @@
+package LAB_1.Part_3;
+
+// 3
+public class SubSingleton extends Singleton {
+
+    private SubSingleton(String nameParam) {
+        super(nameParam);
+    }
+}
---
+++ LAB_1/Part_3/TEST.java
@@ -0,0 +1,9 @@
+package LAB_1.Part_3;
+
+// 3
+class TEST {
+	public static void main(String[] args) {
+		// test getInstance() method of SubSingleton class
+		System.out.println(SubSingleton.getInstance("I1"));
+	}
+}
